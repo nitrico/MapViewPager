d9383a4539f1804324df3e3478230c781dc5a122
Miguel Ángel Moreno <nitrico@gmail.com>
2016-01-28 09:00:39 +0100

    Added sample apk

--- mapviewpager/src/main/java/com/github/nitrico/mapviewpager/MapViewPager.java
+++ mapviewpager/src/main/java/com/github/nitrico/mapviewpager/MapViewPager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Miguel Ángel Moreno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.nitrico.mapviewpager;
 
 import android.content.Context;
@@ -82,7 +98,7 @@ public class MapViewPager extends FrameLayout implements OnMapReadyCallback {
 
     public MapViewPager(Context context) {
         super(context);
-        // not calling initialize(context, null) to use it with Builder
+        // not calling initialize(context) to use it with Builder
     }
 
     public MapViewPager(Context context, AttributeSet attrs) {
